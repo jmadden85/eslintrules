65e488b783a357888f77f7d8bb19b8d52bd4cc0c
jmadden85 <jmadden85@gmail.com>
2015-07-28 16:47:51 -0400

    fix

---
+++ test.js
@@ -0,0 +1,14 @@
+(function() {
+  var test = {
+    int: 1,
+    str: 'testing',
+    key: 'value',
+    bool: true
+  }
+
+  function testing(){
+    console.log( 'working' );
+  }
+
+  var arr = new Array(1,2,3);
+}());
